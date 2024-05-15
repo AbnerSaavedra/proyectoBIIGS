3184ad6cbd06a797d46aef595c4d9241d596b5fd
Ruben <rubdeivis@gmail.com>
2024-05-15 12:11:18 -0400

    Agregar, Listar y parte de editar examenes

--- servicios/servidor.py
+++ servicios/servidor.py
@@ -9,14 +9,15 @@ from pymongo import MongoClient
 app = Flask(__name__, template_folder="../templates")
 app.config['SECRET_KEY'] = "clave secreta"
 
+# Categorías
 @app.route("/categoria/list", methods=["GET"])
-def getList():
+def getCategoriaList():
     elementsList = categorias.find()
 
     return render_template('/categorias/lista.html.jinja', elementsList=elementsList)
 
 @app.route('/categoria/add', methods=['GET', 'POST'])
-def add_element():
+def addCategoria():
     if request.method == "POST":
         nombre = request.form['nombre']
         descripcion = request.form['descripcion']
@@ -25,17 +26,17 @@ def add_element():
             'descripcion': descripcion
         }
         categorias.insert_one(object)
-        return redirect(url_for('getList'))
+        return redirect(url_for('getCategoriaList'))
     return render_template("/categorias/add.html.jinja")
 
 @app.route('/categoria/detail/<id>', methods=['GET'])
-def get_element(id):
+def getCategoria(id):
     oid = ObjectId(id)
     element = categorias.find_one({'_id': oid})
     return render_template('/categorias/detail.html.jinja', element = element)
 
 @app.route('/categoria/update/<id>', methods=['GET', 'POST'])
-def update_element(id):
+def updateCategoria(id):
     oid = ObjectId(id)
     element = categorias.find_one({'_id': oid})
     if request.method == "POST":
@@ -43,13 +44,65 @@ def update_element(id):
         categorias.replace_one({'_id': oid}, 
                                          {'nombre': new_element['nombre'],
                                           'descripcion': new_element['descripcion']})    
-        return redirect(url_for('getList'))
+        return redirect(url_for('getCategoriaList'))
     return render_template("/categorias/update.html.jinja", element=element)
 
 @app.route('/categoria/delete/<id>', methods=['POST'])
-def delete_element(id):
+def deleteCategoria(id):
     oid = ObjectId(id)
     categorias.delete_one({'_id': oid})
+    return redirect(url_for('getList'))
+
+#Examenes
+@app.route("/examen/list", methods=["GET"])
+def getExamenList():
+    elementsList = examenes.find()
+
+    return render_template('/examenes/lista.html.jinja', elementsList=elementsList)
+
+@app.route('/examen/add', methods=['GET', 'POST'])
+def addExamen():
+    if request.method == "POST":
+        nombre = request.form['nombre']
+        categoria = request.form['categoria']
+        tipoMuestra = request.form['tipoMuestra']
+        precio = request.form['precio']
+        indicaciones = request.form['indicaciones']
+        object = {
+            'nombre': nombre,
+            'categoria': categoria,
+            'tipo': tipoMuestra,
+            'precio': precio,
+            'indicaciones': indicaciones
+        }
+        examenes.insert_one(object)
+        return redirect(url_for('getCategoriaList'))
+    listaCategorias = categorias.find()
+    return render_template("/examenes/add.html.jinja", listaCategorias = listaCategorias)
+
+@app.route('/examen/detail/<id>', methods=['GET'])
+def getExamen(id):
+    oid = ObjectId(id)
+    element = examenes.find_one({'_id': oid})
+    return render_template('/examenes/detail.html.jinja', element = element)
+
+@app.route('/examen/update/<id>', methods=['GET', 'POST'])
+def updateExamen(id):
+    oid = ObjectId(id)
+    element = examenes.find_one({'_id': oid})
+    if request.method == "POST":
+        new_element = request.form
+        examenes.replace_one({'_id': oid}, 
+                                         {'nombre': new_element['nombre'],
+                                          'descripcion': new_element['descripcion']})    
+        return redirect(url_for('getExamenList'))
+    listaCategorias = categorias.find()
+    return render_template("/examenes/update.html.jinja", element=element, listaCategorias = listaCategorias, id=id)
+
+@app.route('/examen/delete/<id>', methods=['POST'])
+def deleteExamen(id):
+    oid = ObjectId(id)
+    examenes.delete_one({'_id': oid})
     return redirect(url_for('getList'))
 
 if __name__ == "__main__":
